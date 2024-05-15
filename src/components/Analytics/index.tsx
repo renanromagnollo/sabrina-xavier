fc7ab0214bb2e09bdb30faee792f7498268a8c26
renanromagnollo <renanromagnoli@yahoo.com.br>
2024-05-15 17:44:45 -0300

    feat: Google Analytics added

---
+++ src/components/Analytics/index.tsx
@@ -0,0 +1,17 @@
+
+export default function Analytics(){
+    return(
+        <>
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING}`}></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.GA_TRACKING}');
+            `
+          }}/>
+        </>
+    )
+}
