9368de61ab54511d3ca5f66c970fa7f0807d7af0
renanromagnollo <renanromagnoli@yahoo.com.br>
2024-08-09 19:21:18 -0300

    feat: studio loading added

--- src/app/components/pages/Home/Studio/index.tsx
+++ src/app/components/pages/Home/Studio/index.tsx
@@ -1,15 +1,14 @@
-'use client'
+"use client";
 
-import { DataContext } from "@/context/data-context"
-import { HygraphAboutStudioProps } from "@/types/hygraph-types"
-import { RichTextHygraph } from "@/utils/richtTextHygraph"
-import Image from "next/image"
-import { useContext } from "react"
-import styled from "styled-components"
+import { LoadingCircle } from "@/components/Loadings/LoadingCircle";
+import { DataContext } from "@/context/data-context";
+import { HygraphAboutStudioProps } from "@/types/hygraph-types";
+import { RichTextHygraph } from "@/utils/richtTextHygraph";
+import Image from "next/image";
+import { useContext } from "react";
+import styled from "styled-components";
 
-interface StudioProps {
-
-}
+interface StudioProps {}
 
 const Container = styled.section`
   width: 100vw;
@@ -17,44 +16,83 @@ const Container = styled.section`
   margin: 40px 0;
   padding: 60px;
   display: flex;
+  justify-content: center;
   gap: 30px;
-  color: ${({theme}) => theme.colors.dark.light};
-  background-color: ${({theme}) => theme.colors.light.dark};
-`
+  color: ${({ theme }) => theme.colors.dark.light};
+  background-color: ${({ theme }) => theme.colors.light.dark};
+`;
 
 const TextsArea = styled.div`
+  opacity: 0;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   padding: 30px 0;
+  animation: enter 1.5s ease-out forwards;
 
   p {
     text-align: justify;
   }
 
-`
-export function Studio(props : StudioProps){
-  const {hygraphHome} = useContext(DataContext)
+  @keyframes enter {
+    from {
+      margin-left: 20px;
+      opacity: 0;
+    }
+    to {
+      margin-left: 0;
+      opacity: 1;
+    }
+  }
+`;
 
-  const aboutStudioHygraph: HygraphAboutStudioProps = hygraphHome?.aboutStudio
-    return(
-        <Container>
-          <div>
-            <Image 
-              src={aboutStudioHygraph?.imageMain.url ?? `http://picsum.photos//${572}/${296}`}
+const ImageArea = styled.div`
+  opacity: 0;
+  animation: enter 1s ease-out forwards;
+
+  @keyframes enter {
+    from {
+      transform: scale(0.8);
+    }
+    to {
+      transform: scale(1);
+      opacity: 1;
+    }
+  }
+`;
+export function Studio(props: StudioProps) {
+  const { hygraphHome } = useContext(DataContext);
+
+  const aboutStudioHygraph: HygraphAboutStudioProps = hygraphHome?.aboutStudio;
+  return (
+    <Container>
+      {!aboutStudioHygraph ? (
+        <LoadingCircle />
+      ) : (
+        <>
+          <ImageArea>
+            <Image
+              src={
+                aboutStudioHygraph?.imageMain.url ??
+                `http://picsum.photos//${572}/${296}`
+              }
               alt="studio-image"
               width={572}
               height={296}
               sizes="100%"
-              style={{objectFit: 'cover'}}
+              style={{ objectFit: "cover" }}
               priority
               unoptimized
             />
-          </div>
+          </ImageArea>
           <TextsArea>
             <h2>{aboutStudioHygraph?.title}</h2>
-            <p><RichTextHygraph content={aboutStudioHygraph?.text.raw}/></p>
+            <p>
+              <RichTextHygraph content={aboutStudioHygraph?.text.raw} />
+            </p>
           </TextsArea>
-        </Container>
-    )
+        </>
+      )}
+    </Container>
+  );
 }
