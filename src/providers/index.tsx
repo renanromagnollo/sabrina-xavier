74c277ffa17fae73676ea450839e39077392940d
renanromagnollo <renanromagnoli@yahoo.com.br>
2024-08-16 11:23:31 -0300

    feat: QueryClientProvider added

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -4,6 +4,9 @@ import { ReactNode } from "react";
 import StyledComponentsProvider from "./StyledComponentsProvider";
 import DataProvider from "@/context/data-context";
 import ModalInstagramProvider from "@/context/modal-instagram-context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 interface ProvidersProps {
   children: ReactNode;
@@ -11,9 +14,11 @@ interface ProvidersProps {
 export default function Providers(props: ProvidersProps) {
   return (
     <StyledComponentsProvider>
-      <ModalInstagramProvider>
-        <DataProvider>{props.children}</DataProvider>
-      </ModalInstagramProvider>
+      <QueryClientProvider client={client}>
+        <ModalInstagramProvider>
+          <DataProvider>{props.children}</DataProvider>
+        </ModalInstagramProvider>
+      </QueryClientProvider>
     </StyledComponentsProvider>
   );
 }
