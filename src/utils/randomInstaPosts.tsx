0ce9ba4bf16bbe0f4c410ac781e5358ffe996583
Renan Romagnollo <renanromagnoli@yahoo.com.br>
2024-09-18 17:11:46 -0300

    Closes #3: Instagram and Hygraph productions fetches done

--- src/utils/randomInstaPosts.tsx
+++ src/utils/randomInstaPosts.tsx
@@ -1,12 +1,15 @@
-import { InstagramPostProps } from "@/types/post-instagram-types"
+import { InstagramPostProps } from '@/types/post-instagram-types';
 
-export function randomInstaPosts(listPosts: InstagramPostProps[], number: number): InstagramPostProps[]{
-    // const length = listPosts.length
-    console.log(listPosts)
-    let shuffledPosts = listPosts?
-        .map(value => ({ value, sort: Math.random()})) 
-        .sort((a, b) => a.sort - b.sort)
-        .map(({value}) => value)
-    const sortedList = shuffledPosts?.slice(0, number)
-    return sortedList
+export function randomInstaPosts(
+  listPosts: InstagramPostProps[],
+  number: number
+): InstagramPostProps[] {
+  // const length = listPosts.length
+  console.log(listPosts);
+  let shuffledPosts = listPosts
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+  const sortedList = shuffledPosts?.slice(0, number);
+  return sortedList;
 }
