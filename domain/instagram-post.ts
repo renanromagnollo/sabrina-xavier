ac7012f2515f13a9cd8957dbae6d33e5141caa26
renanromagnollo <renanromagnollo@gmail.com>
2024-10-20 17:20:03 -0300

    feat: add instagram-post domain

---
+++ domain/instagram-post.ts
@@ -0,0 +1,11 @@
+export type InstagramPost = {
+  id: string
+  date: string
+  type: string
+  thumbnail_url?: string
+  img_url: string
+  text: string
+  link: string
+}
+
+//TODO: Criar as outras domains
